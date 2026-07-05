2f405f9387532200339e6391e7b7d92928dc8ef9
chimputu <kafiswegchimputu@gmail.com>
2026-07-05 08:26:46 +0000

    "third Commit"

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,9 @@
-import { createBrowserClient } from '@supabase/ssr';
+// lib/supabase.ts
+'use client'
 
-export const supabase = createBrowserClient(
+import { createClient } from '@supabase/supabase-js'
+
+export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-);
+)
